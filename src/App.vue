2ba38de8f04fb3d94056f480d82d8a3fec1c42a7
ryotaro <rrr00330066@gmail.com>
2023-12-20 16:19:43 +0900

    Auto stash before checking out "origin/test"

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app style="background-color: #252b30">
+    <v-row justify="center" align="center" style="height: 100vh">
+      <v-col cols="12" sm="8" md="4">
+        <v-card
+          width="1000px"
+          class="mx-auto mt-3"
+          style="background-color: #252b30"
+        >
+          <v-card-title>
+            <h1
+              class="display-1"
+              :style="{
+                color: 'white',
+              }"
+            >
+              出欠管理アプリ
+            </h1>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                prepend-icon="mdi-account-circle"
+                label="ユーザ名"
+                v-model="name"
+                style="background-color: #edfdff"
+              />
+              <v-text-field
+                style="background-color: #edfdff"
+                v-bind:type="showPassword ? 'text' : 'password'"
+                @click:append="showPassword = !showPassword"
+                prepend-icon="mdi-lock"
+                v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                label="パスワード"
+                v-model="password"
+              />
+              <v-btn
+                class="info"
+                :style="{
+                  backgroundColor: '#82B7BE',
+                  color: 'white',
+                }"
+                @click="submit"
+                >ログイン</v-btn
+              >
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      showPassword: false,
+      name: "",
+      password: "",
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.name, this.password);
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<template>トップ画面</template>
+
+<script>
+import { defineComponent } from "vue";
+
+// Components
+export default defineComponent({
+  name: "LoginView",
+});
+</script>
